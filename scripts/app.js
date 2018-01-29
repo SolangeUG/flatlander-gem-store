db64b76bb8e79cc7b039e86bae0da2667eb763b5
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-01-29 17:02:30 +0100

    Add image array to our product array

--- scripts/app.js
+++ scripts/app.js
@@ -11,14 +11,50 @@
 			price: 2.95,
 			description: 'A polyhedron with twelve flat faces.',
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			images: [
+				{
+					full: 'images/dodecahedron-24.jpg',
+					thumb: 'images/dodecahedron-thumb.jpg'
+				},
+				{
+					full: 'images/dodecahedron-32.jpg',
+					thumb: 'images/dodecahedron-thumb.jpg'
+				},
+				{
+					full: 'images/dodecahedron-64.jpg',
+					thumb: 'images/dodecahedron-thumb.jpg'
+				},
+				{
+					full: 'images/dodecahedron-128.jpg',
+					thumb: 'images/dodecahedron-thumb.jpg'
+				},
+			]
 		},
 		{
 			name: 'Pentagonal Gem',
 			price: 5.95,
 			description: 'A polyhedron with five flat faces.',
 			canPurchase: false,
-			soldOut: false
+			soldOut: false,
+			images: [
+				{
+					full: 'images/pentagon-24.jpg',
+					thumb: 'images/pentagon-thumb.jpg'
+				},
+				{
+					full: 'images/pentagon-32.jpg',
+					thumb: 'images/pentagon-thumb.jpg'
+				},
+				{
+					full: 'images/pentagon-64.jpg',
+					thumb: 'images/pentagon-thumb.jpg'
+				},
+				{
+					full: 'images/pentagon-128.jpg',
+					thumb: 'images/pentagons-thumb.jpg'
+				},
+			]
 		}
 	];
 
