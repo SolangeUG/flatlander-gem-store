1c92df7f19a4457accf1a91e503b449dc66a5485
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-01-30 16:26:37 +0100

    Use service injection

--- scripts/app.js
+++ scripts/app.js
@@ -3,10 +3,21 @@
 	var app = angular.module('store', [ 'store-products' ]);
 
 	// a controller defines a set of (specialized) operations
-	app.controller('StoreController', function() {
-    // products = property of the StoreController
-		this.products = gems;
-	});
+	app.controller('StoreController', ['http', function($http) {
+  	// using service injection
+		var store = this;
+
+		// initialize our products array to an empty array
+		store = [];
+
+		// products getting loaded remotely from an API
+		$http.get('/data/products.json')
+			.success(function(data) {
+				// here, we can't use this.products directly,
+				// because 'this' refers to the the http service here.
+				store.products = data;
+			});
+	}]);
 
 	// a controller for our review functionality
 	app.controller('ReviewController', function() {
@@ -17,83 +28,5 @@
 			this.review = {};
 		};
 	});
-
-	// gems defines our web store data
-	var gems = [
-		{
-			name: 'Dodecahedron',
-			price: 2.95,
-			description: 'A polyhedron with twelve flat faces.',
-			canPurchase: true,
-			soldOut: false,
-			images: [
-				{
-					full: 'images/dodecahedron-24.jpg',
-					thumb: 'images/dodecahedron-thumb.jpg'
-				},
-				{
-					full: 'images/dodecahedron-32.jpg',
-					thumb: 'images/dodecahedron-thumb.jpg'
-				},
-				{
-					full: 'images/dodecahedron-64.jpg',
-					thumb: 'images/dodecahedron-thumb.jpg'
-				},
-				{
-					full: 'images/dodecahedron-128.jpg',
-					thumb: 'images/dodecahedron-thumb.jpg'
-				},
-			],
-			reviews: [
-				{
-					stars: 5,
-					body: 'I love this product',
-					author: 'joe@thomas.com'
-				},
-				{
-					stars: 1,
-					body: 'This product sucks',
-					author: 'tim@hater.com'
-				}
-			]
-		},
-		{
-			name: 'Pentagonal Gem',
-			price: 5.95,
-			description: 'A polyhedron with five flat faces.',
-			canPurchase: false,
-			soldOut: false,
-			images: [
-				{
-					full: 'images/pentagon-24.jpg',
-					thumb: 'images/pentagon-thumb.jpg'
-				},
-				{
-					full: 'images/pentagon-32.jpg',
-					thumb: 'images/pentagon-thumb.jpg'
-				},
-				{
-					full: 'images/pentagon-64.jpg',
-					thumb: 'images/pentagon-thumb.jpg'
-				},
-				{
-					full: 'images/pentagon-128.jpg',
-					thumb: 'images/pentagons-thumb.jpg'
-				},
-			],
-			reviews: [
-				{
-					stars: 5,
-					body: 'I totally love this!',
-					author: 'helen@love.com'
-				},
-				{
-					stars: 4,
-					body: 'Not bad at all',
-					author: 'john@doe.com'
-				}
-			]
-		}
-	];
 
 })();
