ca8ea53e49a698a56ff5d5512949dd04f52c52ca
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-01-31 12:50:11 +0100

    Simplify angular application

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,19 @@
 (function() {
 	// app defines the business logic of our web store
-	var app = angular.module('store', [ 'store-products' ]);
+	var app = angular.module('gemStore', ['store-directives']);
 
 	// a controller defines a set of (specialized) operations
 	app.controller('StoreController', ['$http', function($http) {
   	// using service injection
 		var store = this;
-
 		// initialize our products array to an empty array
-		store = [];
-
+		store.products = [];
 		// products getting loaded remotely from an API
-		$http.get('/data/products.json')
-			.success(function(data) {
+		$http.get('../data/products.json')
+			.then(function(response) {
 				// here, we can't use this.products directly,
 				// because 'this' refers to the the http service here.
-				store.products = data;
+				store.products = response.data;
 			});
 	}]);
 
